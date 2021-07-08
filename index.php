a62142df7dca1405aabee2b0bc42855409ad9060
mattiadebonis <mattia.debonis@gmail.com>
2021-07-08 15:43:07 +0200

    Crea e stampa film

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-    include __DIR__ . "../partial/movie.php";
+    include __DIR__ . "/partial/movie.php";
 ?>
 
 
@@ -12,6 +12,19 @@
     <title>Document</title>
 </head>
 <body>
+
+    <?php
+        $starwarsIV = new Movie("Star Wars Episodio IV - Una nuova speranza","Fantascienza", 5, "Il film vede come protagonisti Luke Skywalker, Obi-Wan Kenobi, Han Solo, la principessa Leia Organa, R2-D2, C-3PO e l'Alleanza Ribelle che cercano di distruggere la stazione spaziale corazzata imperiale Morte Nera." , 1977 );
+
+        $starwarsV = new Movie("Star Wars Episodio V - L'impero colpisce ancora","Fantascienza", 5, "L'Impero colpisce ancora (The Empire Strikes Back) è un film del 1980, ed è il secondo film (in ordine di uscita) della saga di Star Wars." , 1980);
+        
+        echo $starwarsIV->$title;
     
+        
+        
+        
+        
+              
+    ?>
 </body>
 </html>
